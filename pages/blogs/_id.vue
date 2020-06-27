a37b3bd2cdab33fddd2d54d374a28c25026f0611
PrijalBista <pri1.zzal@gmail.com>
2020-06-27 23:26:12 +0545

    Add blogpost/id  show single blog post Request blog post by id and get the data for single post show dynamically the post info with fallback img if blogpost doesnt have photos also request latest blogposts (using same endpoints to get list of blogposts) filter the results so that suggested blog post doesnt have currently viewd blog post using filter(el =>  el.id != this.$route.params.id)

--- pages/blogs/_id.vue
+++ pages/blogs/_id.vue
@@ -1,32 +1,29 @@
 <template>
   <div>
-    <Preview image="http://via.placeholder.com/1282x720" />
+    <Preview :image="previewImage" />
     <div class="container py-5">
       <div class="row">
         <div class="col-12 col-12 col-lg-8 mb-5">
           <div class="card is-radiusless">
             <div class="card-body is-radiusless py-4">
-              <h3 class="font-weight-normal">Lorem ipsum dolor sit amet</h3>
+              <h3 class="font-weight-normal">{{ post.title }}</h3>
               <p class="text-muted">
                 <i class="las la-user-tie"></i> By: Admin |
-                <i class="las la-calendar"></i> 24 May, 2020
+                <i class="las la-calendar"></i> {{ post.updated_at }}
               </p>
 
-              <img src="https://via.placeholder.com/610x350" class="img-fluid" width="100%" />
+              <img :src="post.photos[0] ? post.photos[0].photo_url: defaultMainPostPhotoUrl" class="img-fluid" width="100%" />
 
-              <p class="mt-4">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                <br />
-                <br />Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              <p class="mt-4" v-html="post.content">
               </p>
 
               <div class="row mt-4">
-                <div class="col-12 col-sm-6 col-md-4 mb-3" v-for="i in 4" :key="i">
+                <div class="col-12 col-sm-6 col-md-4 mb-3" v-for="photo in post.photos.slice(1)" :key="photo.id">
                   <!-- <img src="https://via.placeholder.com/610x350" class="img-fluid" /> -->
                   <div
                     class="blog-image p-3"
-                    style="background-image: url(https://via.placeholder.com/610x350)"
-                    @click="gallery"
+                    :style="`background-image: url(${photo.photo_url})`"
+                    @click="gallery(photo.photo_url)"
                   >
                     <div class="d-flex align-items-center justify-content-center">
                       <h1 class="display-4 text-white">
@@ -44,13 +41,13 @@
           <h5>LATEST POSTS</h5>
           <hr />
 
-          <div class="card mb-3 is-borderless" v-for="i in 6" :key="i">
+          <div class="card mb-3 is-borderless" v-for="post in latestPosts.slice(0,6)" :key="post.id">
             <div class="row no-gutters">
               <div class="col-4 col-md-4">
                 <nuxt-link
                   tag="div"
                   class="card-img is-radiusless"
-                  style="background-image: url(https://via.placeholder.com/85x60)"
+                  :style="`background-image: url(${(post.photos[0] ? post.photos[0].photo_url : defaultLatestPostsPhotoUrl)})`"
                   to="/blogs/help-girls-to-get-education"
                 ></nuxt-link>
                 <!-- <div
@@ -61,9 +58,9 @@
               <div class="col-8 col-md-8">
                 <div class="card-body">
                   <p class="card-text text-muted">
-                    <nuxt-link to="/blogs/help-girls-to-get-education">Help girls to get education</nuxt-link>
+                    <nuxt-link :to="`/blogs/${post.id}`">{{ stripTitle(post.title) }}</nuxt-link>
                     <br />
-                    <small class="text-muted">November, 15, 2016</small>
+                    <small class="text-muted">{{ post.updated_at }}</small>
                   </p>
                 </div>
               </div>
@@ -79,10 +76,55 @@
 import Preview from "@/components/UI/Preview";
 
 export default {
-  methods: {
-    gallery() {
-      $("#preview").modal("show");
+  props: ['id'],
+
+  data() {
+    return {
+      post: {
+        title: '',
+        content: '',
+        photos: [],
+      },
+      defaultMainPostPhotoUrl:'https://via.placeholder.com/610x350',
+      defaultLatestPostsPhotoUrl: 'https://via.placeholder.com/85x60',
+      previewImage: 'http://via.placeholder.com/1282x720',
+      latestPosts: [],
     }
+  },
+
+  methods: {
+    gallery(photourl) {
+      this.previewImage = photourl;
+      $("#preview").modal("show");
+    },
+
+    stripTitle(title) {
+      if(title.length > 24) {
+        title = title.substring(0, 24) + ' ...';
+      }
+      return title;
+    },
+  },
+
+  created() {
+    this.$axios.get(`api/webui/blogposts/show/${this.$route.params.id}`)
+      .then(res => {
+        this.post = res.data.data;
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+
+    // populate latest blog posts
+    this.$axios.get('api/webui/blogposts')
+      .then(res => {
+        let latestPosts = res.data.data;
+        latestPosts = latestPosts.filter(el => el.id != this.$route.params.id);
+        this.latestPosts = latestPosts;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
 
   components: {
