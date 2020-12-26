6167819425ad93e36242def3242e3c6c5ed5b17e
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-12-26 15:43:02 +0545

    image -> placeholder

--- components/index/Help.vue
+++ components/index/Help.vue
@@ -4,26 +4,54 @@
       <div class="container py-5">
         <div class="text-center mb-5">
           <h1>HOW CAN YOU HELP US?</h1>
-          <p
-            class="mb-5"
-          >In order for us to carry out this valuable work we are in urgent need of donations.</p>
+          <p class="mb-5">
+            In order for us to carry out this valuable work we are in urgent
+            need of donations.
+          </p>
         </div>
 
         <div class="row">
           <div class="col-12 col-md-4">
             <h4 class="mb-3">DONATOR</h4>
-            <p>People like you give what you can to your favorite projects; you feel great when you get updates about how your money is put to work by us.</p>
-            <nuxt-link to="/aboutus" tag="button" class="btn btn-outline-white mt-2">LEARN MORE</nuxt-link>
+            <p>
+              People like you give what you can to your favorite projects; you
+              feel great when you get updates about how your money is put to
+              work by us.
+            </p>
+            <nuxt-link
+              to="/aboutus"
+              tag="button"
+              class="btn btn-outline-white mt-2"
+              >LEARN MORE</nuxt-link
+            >
           </div>
           <div class="col-12 col-md-4">
             <h4 class="mb-3">VOLUNTEER</h4>
-            <p>Be a volunteer and reap the reward: If you want to help the poorest, you are in the right place here. Join our team and let us work together.</p>
-            <nuxt-link to="/aboutus" tag="button" class="btn btn-outline-white mt-2">LEARN MORE</nuxt-link>
+            <p>
+              Be a volunteer and reap the reward: If you want to help the
+              poorest, you are in the right place here. Join our team and let us
+              work together.
+            </p>
+            <nuxt-link
+              to="/aboutus"
+              tag="button"
+              class="btn btn-outline-white mt-2"
+              >LEARN MORE</nuxt-link
+            >
           </div>
           <div class="col-12 col-md-4">
             <h4 class="mb-3">ADVOCATE</h4>
-            <p>Defend and speak up on behalf of the vulnerable and suffering! Together, we can intercede on behalf of those who are suffering, without protection.</p>
-            <nuxt-link to="/aboutus" tag="button" class="btn btn-outline-white mt-2">LEARN MORE</nuxt-link>
+            <p>
+              Defend and speak up on behalf of the vulnerable and suffering!
+              Together, we can intercede on behalf of those who are suffering,
+              without protection.
+            </p>
+            <nuxt-link
+              to="/aboutus"
+              tag="button"
+              class="btn btn-outline-white mt-2"
+              >LEARN MORE</nuxt-link
+            >
           </div>
         </div>
       </div>
@@ -36,8 +64,8 @@
 
 section {
   // 1920x530
-  // background-image: url(http://via.placeholder.com/1024x283);
-  background-image: url("/images/cover.jpg");
+  background-image: url(http://via.placeholder.com/1024x283);
+  // background-image: url("/images/cover.jpg");
   background-position: center;
   background-attachment: fixed;
   background-repeat: no-repeat;
