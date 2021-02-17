794d7e314d3b990b79564f66a78ad53df4a355ff
ashish336b <ashish336b@gmail.com>
2021-02-17 20:41:48 +0545

    title changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,71 @@
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
-    title: 'Better Life Social Organization',
-    meta: [{
-        charset: 'utf-8'
+    title: "Org-Name",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
+    link: [
       {
-        rel: 'stylesheet',
-        type: 'text/css',
-        href: '/css/line-awesome.min.css'
-      },
-      {
-        rel: 'stylesheet',
-        href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto&family=Roboto+Mono:wght@500&family=Raleway:wght@400;500;600;700&display=swap"
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
       },
       {
         rel: "stylesheet",
-        href: "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
-        integrity: "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk",
-        crossorigin: "anonymous"
+        type: "text/css",
+        href: "/css/line-awesome.min.css"
       },
-
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto&family=Roboto+Mono:wght@500&family=Raleway:wght@400;500;600;700&display=swap"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+        integrity:
+          "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk",
+        crossorigin: "anonymous"
+      }
     ],
 
-    script: [{
+    script: [
+      {
         src: "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-        integrity: "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
+        integrity:
+          "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
         crossorigin: "anonymous",
         body: true
       },
       {
-        src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-        integrity: "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo",
+        src:
+          "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+        integrity:
+          "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo",
         crossorigin: "anonymous",
         body: true
       },
       {
-        src: "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
-        integrity: "sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI",
+        src:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
+        integrity:
+          "sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI",
         crossorigin: "anonymous",
         body: true
       }
@@ -65,7 +75,7 @@ export default {
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#fff'
+    color: "#fff"
   },
   /*
    ** Global CSS
@@ -92,4 +102,4 @@ export default {
      */
     extend(config, ctx) {}
   }
-}
+};
